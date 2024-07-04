3fd171a89f56d3847eabfe85f8ea7407575c65d6
nagiyu <otonagiyu@gmail.com>
2024-07-04 15:24:10 +0900

    chore: Add DotNetFramework project files and views

---
+++ DotNetFramework/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotNetFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // カスタムヘッダーを追加
+            Response.AddHeader("X-Custom-Header", "This is a custom header value");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
