8e4c8d113e611b50790cd3536a0fa06a055d325e
nagiyu <otonagiyu@gmail.com>
2024-07-04 15:24:20 +0900

    chore: Add DotNetCore project files and views

---
+++ DotNetCore/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using DotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DotNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // カスタムヘッダーを追加
+            Response.Headers.Append("X-Custom-Header", "This is a custom header value");
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
